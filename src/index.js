22c86d10c023d5d833b8f0b3cb74482744558f56
kaceyluo <luo1111111111111@gmail.com>
2022-06-30 16:18:00 +0800

    final

--- src/index.js
+++ src/index.js
@@ -1,2 +1,22 @@
 import x from './x.js'
-console.log('hi')
+import png from './assets/1.png'
+
+const div = document.getElementById('app')
+div.innerHTML = `
+<img src="${png}">`
+
+
+//懒加载
+const button = document.createElement('button')
+button.innerText = '懒加载'
+button.onclick = () => {
+    const promise = import('./lazy')
+    Promise.then((module) => {
+        const fn = module.default
+        fn()
+    }, () => {
+        console.log('模块加载错误')
+    })
+}
+
+div.appendChild(button)
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,21 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const base = require("./webpack.config.base.js");
 
 module.exports = {
-  mode: 'development',
-  entry: './src/index.js',
-  output: {
-    filename: '[name].[contenthash].js',
+  ...base,
+  devtool: "inline-source-map",
+  sevServer: {
+    contentBase: "./dist"
   },
+  module: {
+    rules: [
+      ...base.module.rules,
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+
 };
