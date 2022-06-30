c25a3538e362ad904cd61a0443a3f860a1a7f672
kaceyluo <luo1111111111111@gmail.com>
2022-06-30 16:22:57 +0800

    modify

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import x from './x.js'
 import png from './assets/1.png'
+console.log(png)
 
 const div = document.getElementById('app')
+console.log('div')
+console.log(div)
 div.innerHTML = `
-<img src="${png}">`
+  <img src="${png}">
+`
 
-
-//懒加载
 const button = document.createElement('button')
 button.innerText = '懒加载'
 button.onclick = () => {
     const promise = import('./lazy')
-    Promise.then((module) => {
+    promise.then((module) => {
         const fn = module.default
         fn()
     }, () => {
